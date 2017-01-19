0bbc884e8e48c7811360add5a247b6d84efe9a9c
Erianne Viray <erialv@uw.edu>
2017-01-19 15:20:47 -0800

    Add module 9 for participation

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,41 +1,55 @@
 # Load R's "USPersonalExpenditure" dataset using the "data()" function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
+is.data.frame("USPersonalExpenditure")
 
+# View(USPersonalExpenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable as an argument
 # to the `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+US.pe <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(US.pe) # "X.USPersonalExpenditure."
 
 # Why are they so strange? Think about whether you could use a number like 1940
 # with dollar notation!
 
-# What are the row names of your dataframe?
 
+# What are the row names of your dataframe?
+rownames(US.pe) # "1"
 
 # Create a column "category" that is equal to your rownames
-
+US.pe$category <- rownames(US.pe)
 
 # How much money was spent on personal care in 1940?
-
+print(USPersonalExpenditure)
+care.1940 <- US.pe["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+food.tobacco.1960 <- US.pe["Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
 # Hint: use the `max()` function to find the largest, then compare that to the column
-
+max(food.tobacco.1960)
 
 # Define a function `DetectHighest` that takes in a year as a parameter, and
 # returns the highest spending category of that year
-
+DetectHighest <- function(year) {
+  collumn <- paste0('X',year)
+  return(US.pe$category[US.pe[,collumn] == max(US.pe[,collumn])] )
+}
 
 # Using your function, determine the highest spending category of each year
+
+check.1940 <- DetectHighest(1940)
+check.1945 <- DetectHighest(1945)
+check.1950 <- DetectHighest(1950)
+check.1955 <- DetectHighest(1955)
+check.1960 <- DetectHighest(1960)
+
--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -1,40 +1,57 @@
 # Exercise 6: Husky Football 2015 Season
 # Read in the Husky Football 2015 game data into a variable called `husky.games.2015`
-
+husky.games.2015 <- read.csv('data/huskies_2015.csv', stringsAsFactors = FALSE)
 
 
 # Create a vector of the teams that the Huskies played against during that season
-
+not.huskies <- husky.games.2015$opponent
 
 
 # Create a vector of the their final scores for the games
 # Call this variable `husky.scores`
-
+husky.scores <- husky.games.2015$uw_score
 
 
 # Create 2 variables called `rushing.yards` and `passing.yards` to represent the yards the Huskies rushed and passed
-
+rushing.yards <- husky.games.2015$rushing_yards
+passing.yards <- husky.games.2015$passing_yards
 
 
 # Create a variable called `combined.yards` that is the total yardage of the Huskies for each game
-
+combined.yards <- rushing.yards + passing.yards
 
 
 # Add the `combined.yards` vector to the dataframe with the column name being `combined`
-
+husky.games.2015["combined"] <- combined.yards
 
 
 # Write the dataframe to a new `.csv` file called `huskies_2015_improved.csv` into the `data directory`
-
+write.csv(husky.games.2015, "huskies_2015_improved.csv")
 
 
 # What is the score of the game where the Huskies had the most combined yards?
-
+score.with.most.yards <- husky.scores[combined.yards == max(combined.yards)]
 
 
 # Define a function `MostYardsScore` that takes in an argument `games` (a data frame) and
 # returns a descriptive sentence about the game that was played that had the most yards scored in it.
-
+MostYardsScore <- function(games) {
+  date <- games$date
+  opponent <- games$uw_score 
+  highet.score <- games$opponent 
+  
+  rushing.yards <- games$rushing_yards
+  passing.yards <- games$passing_yards
+  combined.yards <- passing.yards + rushing.yards
+  
+  most.yards <- max(combined.yards)
+  opponent <- opponents[combined.yards == most.yards]
+  date <- dates[combined.yards == most.yards]
+  highest.score <- scores[combined.yards == most.yards]
+  
+  returns(paste("The game that the Huskies had the most yards was against",
+                opponent, "on", date, "where they scored", highest.score, "points!"))
+}
 
 
 # Challenge!
